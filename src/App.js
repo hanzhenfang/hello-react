9b8276f474ad87fcb5041245282f7ba10651979f
hanzhenfang <hanzhenfffang@gmail.com>
2022-03-30 15:43:05 +0800

    h

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function App() {
     return function () {
       if (CD) {       //ok,当你想放技能的时候，你需要判断是否在冷却时间内，如果在，对不起不能放!
         console.log("不行,cd中")
-        return false
+        return false; //这里写你null， 空值都可以
       }
-      CD = true;       //我准备放技能了,
+      CD = true; //这一步我的技能还没放，即将要释放。因为setTimout是异步执行，所以cd在两秒以后才会被修改,这一步是限制用户频繁点击技能键时，让函数返回一个空值。
       setTimeout(() => {   //现在没有进入cd，当你放技能的时候然后开始启用
         console.log("冲啊!");
+        CD = false; //ok,现在我技能释放完毕，把cd的属性清空。
         fn();
-        // CD = false;    //在两秒之内我的技能CD无法释放
       }, delay);
     }
   }
--- src/pages/todoList/todolist.js
+++ src/pages/todoList/todolist.js
@@ -1,10 +1,9 @@
 import React from "react";
 import './todoList.css';
-import { useReducer, useRef, useState } from "react";
+import { useReducer, useRef } from "react";
 
 export default function TodoList() {
   const userInput = useRef(null);
-  const [showBtn, setShowBtn] = useState(false)
   const [state, dispatch] = useReducer(reducer, [
     { id: 1, content: "敲代码" },
     { id: 2, content: "吃饭" },
@@ -45,13 +44,20 @@ export default function TodoList() {
     }
   }
 
-  const handleMouse = (flag) => {
-    return (e) => {
-      const flag1 = flag - 1;
-      if (e.target.id == flag1) {
-        setShowBtn(true)
-      }
-    }
+  const handleMouseEnter = (e) => {
+    console.log(e.target.id)
+    const id = e.target.id
+    const li = document.getElementById(id)
+    li.style.backgroundColor = "pink";
+    li.querySelector("button").style.opacity = "1";
+  }
+
+  const handleMouseLeave = (e) => {
+    console.log(e.target)
+    const id = e.target.id
+    const li = document.getElementById(id)
+    li.style.backgroundColor = "white";
+    li.querySelector("button").style.opacity = "0";
   }
 
 
@@ -82,9 +88,9 @@ export default function TodoList() {
                 return (
                   <li key={iterm.id}
                     id={index}
-                    onMouseEnter={handleMouse(iterm.id)}
-                    onMouseLeave={handleMouse(iterm.id)}
-                    style={{ backgroundColor: showBtn ? "gray" : "white" }}
+                    style={{ backgroundColor: "white" }}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
                   >
                     <label>
                       <input type="checkbox" />
@@ -92,7 +98,7 @@ export default function TodoList() {
                     </label>
                     <button id={index}
                       onClick={(e) => { dispatch({ type: "dec", index }) }}
-                      style={{ opacity: showBtn ? 1 : 0 }}
+                      style={{ opacity: 0 }}
                     >
                       删除</button>
                   </li>
